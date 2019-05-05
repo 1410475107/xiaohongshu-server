e30a6460a0caf33773ed2128b1ee63847aa0fe1c
1410475107 <1410475107@qq.com>
2019-05-05 17:28:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+app.get('/a', (req, res) => {
+    res.send('Hello World!');
+});
 app.listen(port, () => {
     console.log('Example app listening on port port!');
 });
